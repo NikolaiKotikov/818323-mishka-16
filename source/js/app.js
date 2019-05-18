34b9951de9e2c17d556077bb5cf6c13fa82dd90e
Николай <nikolai.kotikov@gmail.com>
2019-05-18 05:38:10 +0300

    реализовал открывание попапов

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,9 @@
 var navButtonState = document.querySelector(".header__menu-button");
 var headerListSite = document.querySelector(".header__list--site");
 var headerListUser = document.querySelector(".header__list--user");
+var showPopup = document.querySelectorAll(".js-popup-show");
+var popup = document.querySelector(".popup");
+var bg = document.querySelector(".page__background");
 
 headerListSite.classList.add("header__list--hidden");
 headerListUser.classList.add("header__list--hidden");
@@ -18,4 +21,26 @@ navButtonState.addEventListener("click", function (evt) {
     headerListSite.classList.remove("header__list--hidden");
     headerListUser.classList.remove("header__list--hidden");
   }
-})
+});
+
+for (var i=0; i <= showPopup.length -1; i++) {
+  showPopup[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("visually-hidden");
+    bg.classList.remove("page__background--hidden");
+  });
+}
+
+bg.addEventListener("click", function(evt) {
+  popup.classList.add("visually-hidden");
+  bg.classList.add("page__background--hidden");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    popup.classList.add("visually-hidden");
+    bg.classList.add("page__background--hidden");
+  }
+});
+
